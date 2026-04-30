ce55dc773cb44065b128437482a0a6d27ec72f93
Thiago Dantas <eng.thiagohpdantas@gmail.com>
2026-04-30 17:20:49 -0300

    aula-6: metodos e mutabilidade

--- banking/Person.ts
+++ banking/Person.ts
@@ -8,4 +8,22 @@ export class Person {
     this.lastName = lastName;
     this.birthDate = birthDate;
   }
+
+  getFullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getAge(): number {
+    const today = new Date();
+    const age = today.getFullYear() - this.birthDate.getFullYear();
+    const isBirthdayPassed =
+      today.getMonth() > this.birthDate.getMonth() ||
+      (today.getMonth() === this.birthDate.getMonth() &&
+        today.getDate() >= this.birthDate.getDate());
+    return isBirthdayPassed ? age : age - 1;
+  }
+
+  updateFirstName(firstName: string): void {
+    this.firstName = firstName;
+  }
 }
--- index.ts
+++ index.ts
@@ -3,7 +3,11 @@ import { Person } from "./banking/Person";
 function main() {
   const maria = new Person("Maria", "da Silva", new Date("1999-01-15"));
 
-  console.log(maria);
+  console.log(maria.getFullName());
+
+  maria.updateFirstName("Maria Clara");
+
+  console.log(maria.getFullName());
 }
 
 main();
