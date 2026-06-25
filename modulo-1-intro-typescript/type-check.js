112e68f3db585ed134d7528f2bacb19f19bc839e
Thiago Dantas <eng.thiagohpdantas@gmail.com>
2026-06-25 17:34:30 -0300

    modulo-1-aula-6: dynamic vs static type system

---
+++ modulo-1-intro-typescript/type-check.js
@@ -0,0 +1,10 @@
+// tipagem dinâmica
+// Dynamically Typed
+
+let message = "Hello World";
+
+message = 6;
+
+console.log(message.toLocaleUpperCase());
+
+// console.log(typeof message)
---
+++ modulo-1-intro-typescript/type-check.ts
@@ -0,0 +1,8 @@
+// tipagem estática
+// Statically Typed language
+
+let message: string = "Hello World";
+
+// message = 6 // Error: Type 'number' is not assignable to type 'string'
+
+console.log(message.toUpperCase());
