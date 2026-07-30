9daa24352190d3185029baf7e9711767a49e51fd
Thiago Dantas <eng.thiagohpdantas@gmail.com>
2026-07-30 17:20:50 -0300

    modulo-2-aula-6:indexed access types

---
+++ modulo-2-generics-e-manipulacao-de-tipos/indexed-access-types.ts
@@ -0,0 +1,23 @@
+// Indexed Access Types - Acessar uma propriedade específicos em outro tipo
+
+import { publication1 } from "./operator-keyof-type/data";
+import { Publication } from "./operator-keyof-type/models";
+
+const author = publication1["author"];
+// console.log(author);
+
+type authorType = "author";
+type Author = Publication[authorType];
+
+const MyArray = [
+  { surname: "Alice", age: 15 },
+  { surname: "Bob", age: 23 },
+  { surname: "Eve", age: 38 },
+];
+
+type Person = (typeof MyArray)[number];
+
+const person: Person = {
+  surname: "Lucas",
+  age: 34,
+};
